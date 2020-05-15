b279debffd0a6b99cc81e145d9dbe24883312aea
D1egoPolanco <00301417@uca.edu.sv>
2020-05-15 11:52:11 -0600

    JPA Configuration

---
+++ Labo5_JPA/src/test/java/com/uca/capas/app/Labo5JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca.capas.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labo5JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
